c552aebaddd34c000dbca8948c5c4d70b17a71f3
Takuma HANATANI <mail@potato4d.me>
2025-04-15 23:43:31 +0900

    chore: add unchained and mementolan

--- src/masterdata.ts
+++ src/masterdata.ts
@@ -4,56 +4,58 @@ export const SAMPLE_DATA = [
   {
     name: 'Tier1',
     decks: [
-      { name: '天盃龍', image: '/static/deckimages/tenpai-dragon.png' },
+      { name: '青眼', image: '/static/deckimages/blue-eyes.png' },
+      { name: 'スネークアイ', image: '/static/deckimages/snake-eye.png' },
     ],
   },
   {
     name: 'Tier2',
     decks: [
-      { name: '白き森', image: '/static/deckimages/white-forest.png' },
-      { name: 'スネークアイ', image: '/static/deckimages/snake-eye.png' },
+      { name: '破械', image: '/static/deckimages/unchained.png' },
+      { name: 'メメント', image: '/static/deckimages/mementotlan.png' },
     ],
   },
   {
     name: 'Tier3',
     decks: [
-      { name: 'R-ACE', image: '/static/deckimages/r-ace.png' },
-      { name: '千年', image: '/static/deckimages/exodia.png' },
-      { name: '粛声', image: '/static/deckimages/voiceless_voice.png' },
+      { name: '白き森', image: '/static/deckimages/white-forest.png' },
+      { name: 'センチュリオン', image: '/static/deckimages/centur-ion.png' },
+      { name: 'ラビュリンス', image: '/static/deckimages/labrynth.png' },
     ],
   },
   {
     name: 'Tier4',
     decks: [
-      { name: 'ティアラメンツ', image: '/static/deckimages/tearlaments.png' },
-      { name: 'ラビュリンス', image: '/static/deckimages/labrynth.png' },
-      { name: '霊獣', image: '/static/deckimages/ritual-beast.png' },
+      { name: '天盃龍', image: '/static/deckimages/tenpai-dragon.png' },
     ],
   },
 ]
 
 export const INITIAL_AVAILABLE_DECKS: Deck[] = [
+  { name: '霊獣', image: '/static/deckimages/ritual-beast.png' },
   { name: 'ユベル', image: '/static/deckimages/yubel.png' },
+  { name: '烙印', image: '/static/deckimages/branded.png' },
+  { name: 'ティアラメンツ', image: '/static/deckimages/tearlaments.png' },
   { name: 'ドライトロン', image: '/static/deckimages/drytron.png' },
   { name: '竜剣士', image: '/static/deckimages/dracoslayer.png' },
+  { name: '60GS', image: '/static/deckimages/60gs.png' },
+  { name: '炎王', image: '/static/deckimages/fireking.jpg' },
+  { name: 'R-ACE', image: '/static/deckimages/r-ace.png' },
+  { name: '粛声', image: '/static/deckimages/voiceless_voice.png' },
+  { name: '千年', image: '/static/deckimages/exodia.png' },
   { name: 'ギミック・パペット', image: '/static/deckimages/gimmick-puppet.png' },
-  { name: '白き森', image: '/static/deckimages/white-forest.png' },
+  { name: 'メタビート', image: '/static/deckimages/metabeat.png' },
   { name: 'マナドゥム', image: '/static/deckimages/mannadium.png' },
   { name: 'キマイラ', image: '/static/deckimages/chimera.png' },
   { name: '覇王幻奏', image: '/static/deckimages/supreme-melodious.png' },
   { name: '暗黒界', image: '/static/deckimages/darkworld.png' },
   { name: '炎王スネークアイ', image: '/static/deckimages/fireking-snake-eye.png' },
   { name: 'ライトロード', image: '/static/deckimages/lightsworn.png' },
-  { name: 'センチュリオン', image: '/static/deckimages/centur-ion.png' },
   { name: 'インフェルノイド', image: '/static/deckimages/infernoid.png' },
-  { name: '烙印', image: '/static/deckimages/branded.png' },
   { name: 'ヴァリアンツ', image: '/static/deckimages/vaylantz.png' },
   { name: 'スプライト', image: '/static/deckimages/spright.png' },
   { name: '古代の機械', image: '/static/deckimages/ancient-gear.png' },
   { name: '神碑', image: '/static/deckimages/runick.png' },
-
-  { name: '60GS', image: '/static/deckimages/60gs.png' },
-  { name: '炎王', image: '/static/deckimages/fireking.jpg' },
 
   { name: '超重武者', image: '/static/deckimages/superheavy_samurai.png' },
   { name: 'エクソシスター', image: '/static/deckimages/exosister.png' },
